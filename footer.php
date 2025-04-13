73a671fc06cd6d569859c57ce7e7acd3dd8ecef9
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2025-04-14 00:09:48 +0545

    with php

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+//  <!-- Footer -->
+    <footer class="footer">
+      <p>&copy; 2025 Hami Sangai. All rights reserved.</p>
+      <div class="social-icons">
+        <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+    </footer><!-- footer.php -->
+<script src="js/script.js"></script>
+</body>
+</html>
