aba068c2954c87522a857df639c0199876ffe81c
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2025-04-30 21:55:27 +0545

    updating code in dashboard.css for responsive

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -13,7 +13,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
     />
-    <link rel="stylesheet" href="./css/dashboard.css">
+    <link rel="stylesheet" href="css/dashboard.css">
 
   </head>
   <body>
