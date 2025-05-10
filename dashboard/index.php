c91bc8ad98dc41595a2b1433577168f9fd21d6d6
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2025-05-10 23:15:10 +0545

    update code

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,8 @@
 <?php include('partials/header.php'); ?>
-<div class="wrapper">
+<div class="d-flex" style="min-height: 100vh;">
   <?php include('partials/sidebar.php'); ?>
+  <main id="main-content" class="flex-grow-1 p-4" style="margin-left: 250px;">
+  <h1>Welcome to Dashboard</h1>
   <main id="main-content">
   
     <!-- Your content -->
